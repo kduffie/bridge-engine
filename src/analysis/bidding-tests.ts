aa942930747ab4fda008af492e8b6f61f6e08fd1
Kingston Duffie <kduffie@hivepoint.com>
2022-01-10 16:26:00 -0800

    README example

--- src/analysis/bidding-tests.ts
+++ src/analysis/bidding-tests.ts
@@ -5,15 +5,15 @@ import { ALL_INTERPRETER_FACTORIES } from "./interpreters/interpreter-factories"
 
 async function sample1() {
   const analyzer = new BiddingAnalyzer('none', 'N', ALL_INTERPRETER_FACTORIES);
-  await analyzer.onBid(new BidWithSeat('N', 'normal', 1, 'C'));
+  await analyzer.onBid(new BidWithSeat('N', 'normal', 1, 'H'));
   await analyzer.onBid(new BidWithSeat('E', 'pass'));
-  await analyzer.onBid(new BidWithSeat('S', 'normal', 1, 'H'));
+  await analyzer.onBid(new BidWithSeat('S', 'normal', 1, 'S'));
   await analyzer.onBid(new BidWithSeat('W', 'pass'));
-  await analyzer.onBid(new BidWithSeat('N', 'normal', 1, 'S'));
+  await analyzer.onBid(new BidWithSeat('N', 'normal', 2, 'C'));
   await analyzer.onBid(new BidWithSeat('E', 'pass'));
-  await analyzer.onBid(new BidWithSeat('S', 'normal', 2, 'S'));
+  await analyzer.onBid(new BidWithSeat('S', 'normal', 2, 'H'));
   await analyzer.onBid(new BidWithSeat('W', 'pass'));
-  await analyzer.onBid(new BidWithSeat('N', 'normal', 4, 'S'));
+  await analyzer.onBid(new BidWithSeat('N', 'normal', 4, 'H'));
   await analyzer.onBid(new BidWithSeat('E', 'pass'));
   await analyzer.onBid(new BidWithSeat('S', 'pass'));
   await analyzer.onBid(new BidWithSeat('W', 'pass'));
